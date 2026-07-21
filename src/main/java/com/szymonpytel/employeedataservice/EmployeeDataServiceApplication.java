74913e149c810bef13bfeb16baea2402d00da255
Szymon <spytel0128@gmail.com>
2026-07-21 16:46:13 +0200

    add Spring Boot project with Postgres in Docker

---
+++ src/main/java/com/szymonpytel/employeedataservice/EmployeeDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.szymonpytel.employeedataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDataServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/szymonpytel/employeedataservice/EmployeeDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szymonpytel.employeedataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
